1393a2f8b8cf0538bb870603113fa2a32f653274
Sang Lee😁 <leesang9146@gmail.com>
2026-03-05 00:12:09 +0300

    Add bot configuration settings in setting.js

--- setting.js
+++ setting.js
@@ -1,1 +1,11 @@
+//  habari yako 😂 😂 
 
+
+module.exports = {
+  BOT_NAME: 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ ', // ur bot name
+  OWNER_NAME: 'Mr Lee', //your name
+  OWNER_NUMBER: '254116266407', // add your phone number
+  SESSION_ID: '', // add your session ID
+  MODE: "public", // public or private
+  TIMEZONE: 'Africa/Nairobi' // ur timezone
+};
