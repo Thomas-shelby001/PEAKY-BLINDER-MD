89ea2739f6518ea1e09bfa93265ff1e496a6f22a
Sang Lee😁 <leesang9146@gmail.com>
2026-03-10 16:20:02 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -6,6 +6,6 @@ module.exports = {
   OWNER_NAME: 'Mr Lee', //your name
   OWNER_NUMBER: '254116266407', // add your phone number
   SESSION_ID: '', // add your session ID
-  MODE: "public", // public or private
+  MODE: "private", // public or private
   TIMEZONE: 'Africa/Nairobi' // ur timezone
 };
