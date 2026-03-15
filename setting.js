c32b40070b9fe31b882262a87445b7d8a3fa409b
Sang Lee😁 <leesang9146@gmail.com>
2026-03-15 14:22:24 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,11 +1,12 @@
-//  habari yako 😂 😂 
+/*Kelvin Tech*/
 
+// habari yako 😂 😂
 
 module.exports = {
-  BOT_NAME: 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ ', // ur bot name
-  OWNER_NAME: 'Mr Lee', //your name
-  OWNER_NUMBER: '254116266407', // add your phone number
-  SESSION_ID: '', // add your session ID
-  MODE: "private", // public or private
-  TIMEZONE: 'Africa/Nairobi' // ur timezone
+  BOT_NAME: process.env.BOT_NAME || 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ', 
+  OWNER_NAME: process.env.OWNER_NAME || 'Mr Lee', 
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '254116266407', 
+  SESSION_ID: process.env.SESSION_ID || '', // paste here your blinders session ID
+  MODE: process.env.MODE || "public", // public or private
+  TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
 };
