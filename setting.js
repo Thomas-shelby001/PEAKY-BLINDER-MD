35786c08160753f881a2a34c7d63e22f97608dbd
Sang Lee😁 <leesang9146@gmail.com>
2026-03-16 22:27:49 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -7,6 +7,6 @@ module.exports = {
   OWNER_NAME: process.env.OWNER_NAME || 'Mr Lee', 
   OWNER_NUMBER: process.env.OWNER_NUMBER || '254116266407', 
   SESSION_ID: process.env.SESSION_ID || '', // paste here your blinders session ID
-  MODE: process.env.MODE || "public", // public or private
+  MODE: process.env.MODE || "private", // public or private
   TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
 };
