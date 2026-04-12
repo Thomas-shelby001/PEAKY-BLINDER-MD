ffa429f57e871da804ef4585d0fa48a8228bbd37
luffyv2md <testluffyser@gmail.com>
2026-04-12 20:35:32 +0530

    Update setting.js

--- setting.js
+++ setting.js
@@ -3,9 +3,9 @@
 // habari yako 😂 😂
 
 module.exports = {
-  BOT_NAME: process.env.BOT_NAME || 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ', 
-  OWNER_NAME: process.env.OWNER_NAME || 'Mr Lee', 
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '254116266407', 
+  BOT_NAME: process.env.BOT_NAME || 'ʟᴜꜰꜰʏ-ꜱᴇʀ-ᴍᴅ', 
+  OWNER_NAME: process.env.OWNER_NAME || 'Mr 𝚑𝚊𝚗𝚊𝚗', 
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '𝟿𝟷𝟾𝟻𝟿𝟶𝟸𝟷𝟾𝟻𝟿𝟾', 
   SESSION_ID: process.env.SESSION_ID || '', // paste here your blinders session ID
   MODE: process.env.MODE || "private", // public or private
   TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
