97446171f3bdf26d39a62c67b72a56367b55380f
Badboi-repeater <oluwamighty736@gmail.com>
2026-04-29 20:57:35 +0100

    Update SESSION_ID in settings
    Updated SESSION_ID with a new value.

--- setting.js
+++ setting.js
@@ -6,7 +6,7 @@ module.exports = {
   BOT_NAME: process.env.BOT_NAME || 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ', 
   OWNER_NAME: process.env.OWNER_NAME || 'Mr Lee', 
   OWNER_NUMBER: process.env.OWNER_NUMBER || '254116266407', 
-  SESSION_ID: process.env.SESSION_ID || '', // paste here your blinders session ID
+  SESSION_ID: process.env.SESSION_ID || '', // blinder~L6Y1iKgY#OBFj6MqzqFI0gIqTCvNW2J2JzxnNq1Tqi7g36IsxNLk
   MODE: process.env.MODE || "private", // public or private
   TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
 };
